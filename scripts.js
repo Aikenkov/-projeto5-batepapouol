01c00bb4710ee990cc9e752d637c17f096a98b6a
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-25 11:16:33 -0300

    3 tipos de mensagens diferentes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
+let field = document.querySelector('.message-field');
+field.innerHTML = "";
+
 let messages;
 
 let meunome = {
     name: "Fiorinha"
 }
+
 
 login()
 function login() {
@@ -25,25 +29,53 @@ function saveMessages(answer) {
     if (answer.status === 200) {
         console.log("Deuuu boooom");
     }
-
     messages = answer.data;
     renderMessages()
 
 }
 
 function renderMessages() {
-    let field = document.querySelector('.message-field');
-    field.innerHTML = "";
 
     for (let i = 0; i < messages.length; i++) {
-        field.innerHTML += ` 
+
+
+        if (messages[i].type === 'private_message' && to === meunome.name) {
+
+            field.innerHTML += ` 
         <div class="user-message ${messages[i].type}">
         <span class="time">(${messages[i].time})</span>
         <span class="bold">${messages[i].from}</span>
         para 
         <span class="bold">${messages[i].to}:</span>
         ${messages[i].text} </div>`
+        }
+
+
+        if (messages[i].type === 'message') {
+
+            field.innerHTML += ` 
+        <div class="user-message ${messages[i].type}">
+        <span class="time">(${messages[i].time})</span>
+        <span class="bold">${messages[i].from}</span>
+        para 
+        <span class="bold">${messages[i].to}:</span>
+        ${messages[i].text} </div>`
+        }
+
+
+
+        if (messages[i].type === 'status') {
+
+            field.innerHTML += ` 
+        <div class="user-message ${messages[i].type}">
+        <span class="time">(${messages[i].time})</span>
+        <span class="bold">${messages[i].from}</span>
+        
+        ${messages[i].text} </div>`
+        }
     }
 
-    setTimeout(searchMessage, 3000)
 }
+
+setTimeout(searchMessage, 3000)
+
